657c933f95aaa7ed10f702d8f4a15fb1aa585164
rtsnik <meteors3x@gmail.com>
2024-11-24 08:31:20 +0300

    Решение Task5. Коммит №1. Save.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,5 +1,10 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
+import static java.lang.Math.acos;
+import static java.lang.Math.sqrt;
+
 /**
  * Задача поиска площади, величин углов, длин высот, биссектрис, медиан, радиусов вписанной и описанной вокруг
  * треугольника окружностей является центральной в Геометрии.
@@ -10,7 +15,34 @@ package com.walking.intensive.chapter1.task5;
  */
 public class Task5 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println("Test method: getAreaByHeron");
+        System.out.println("Expected: " + 30.0 + "   Recive: " + getAreaByHeron(12, 13, 5));
+        System.out.println("Expected: " + 6.0 + "   Recive: " + getAreaByHeron(3, 4, 5));
+        System.out.println("Expected: " + 120.0 + "   Recive: " + getAreaByHeron(17, 17, 16));
+        System.out.println("Expected: " + 72.30793524918272 + "   Recive: " + getAreaByHeron(12, 13, 14));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(12, 1, 4));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(17, 1, 1));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(17, 1, 1));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(0, 10, 20));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(20, 0, 10));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(10, 20, 0));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(-1, 10, 10));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(10, -1, 10));
+        System.out.println("Expected: " + -1.0 + "   Recive: " + getAreaByHeron(10, 10, -1));
+        System.out.println();
+
+        System.out.println("Test method: getHeights");
+        System.out.println("Expected: " + new double[]{4.615384615384615, 5.0, 12.0}.toString() +
+                            "   Recive: " + getHeights(12, 13, 5).toString());
+        System.out.println("Expected: " + new double[]{2.4, 3.0, 4.0}.toString() +
+                            "   Recive: " + getHeights(3, 4, 5).toString());
+        System.out.println("Expected: " + new double[]{}.toString() + "   Recive: " +
+                            getHeights(17, 1, 1).toString());
+        System.out.println("Expected: " + new double[]{}.toString() +
+                            "   Recive: " + getHeights(0, 10, 12).toString());
+        System.out.println("Expected: " + new double[]{}.toString() +
+                            "   Recive: " + getHeights(20, -10, 10).toString());
+
     }
 
     /**
@@ -23,9 +55,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return -1;
+        }
+        if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
+            return -1;
+        }
+        double p = 0.5 * (a + b + c);
+        return sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     /**
@@ -36,9 +74,17 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return new double[]{};
+        }
+        double p = (a + b + c) / 2;
+        double rootOfSemiPerimeter = p * (p - a) * (p - b) * (p - c);
+        double hA = 2 / a * sqrt(rootOfSemiPerimeter);
+        double hB = 2 / b * sqrt(rootOfSemiPerimeter);
+        double hC = 2 / c * sqrt(rootOfSemiPerimeter);
+        double[] heights = {hA, hB, hC};
+        Arrays.sort(heights);
+        return heights;
     }
 
     /**
@@ -49,9 +95,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return new double[]{};
+        }
+        double mA = 1.0 / 2 * sqrt(2 * c * c + 2 * b * b - a * a);
+        double mB = 1.0 / 2 * sqrt(2 * c * c + 2 * a * a - b * b);
+        double mC = 1.0 / 2 * sqrt(2 * a * a + 2 * b * b - c * c);
+        double[] medians = {mA, mB, mC};
+        Arrays.sort(medians);
+        return medians;
     }
 
     /**
@@ -62,9 +114,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return new double[]{};
+        }
+        double bA = sqrt(b * c * (b * b + 2 * b * c + c * c - a * a)) / (b + c);
+        double bB = sqrt(a * c * (a * a + 2 * a * c + c * c - b * b)) / (a + c);
+        double bC = sqrt(b * a * (b * b + 2 * b * a + a * a - c * c)) / (b + a);
+        double[] bisectors = {bA, bB, bC};
+        Arrays.sort(bisectors);
+        return bisectors;
     }
 
     /**
@@ -75,9 +133,18 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return new double[]{};
+        }
+        if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
+            return new double[]{};
+        }
+        double anglesAlfa = acos((a * a + c * c - b * b) / (2 * a * c));
+        double anglesBeta = acos((a * a + b * b - c * c) / (2 * a * b));
+        double anglesGamma = acos((b * b + c * c - a * a) / (2 * b * c));
+        double[] angles = {anglesAlfa, anglesBeta, anglesGamma};
+        Arrays.sort(angles);
+        return angles;
     }
 
     /**
@@ -88,9 +155,11 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return -1;
+        }
+        double semiPerimeter = (a + b + c) / 2;
+        return getAreaByHeron(a, b, c) / semiPerimeter;
     }
 
     /**
@@ -101,9 +170,10 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return -1;
+        }
+        return (a * b * c) / (4 * getAreaByHeron(a, b, c));
     }
 
     /**
@@ -121,8 +191,11 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+            return -1;
+        }
 
-        return 0; // Заглушка. При реализации - удалить
+        double area;
+        return 0;
     }
 }
